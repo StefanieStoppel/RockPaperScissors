14f3d7cf1fc97645f6717552b73ab7d2dc27b41b
StefanieStoppel <StefanieStoppel@users.noreply.github.com>
2016-06-19 15:44:16 +0200

    Add AngularJS app with controller and service, which send GET request to 'localhost:8080/play/{gameModeId}?hand={playersHand}'; Display result of async request inside index.html; Add three buttons for hands 'rock','paper' and 'scissors'.

---
+++ src/main/resources/static/js/controller/game-controller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+App.controller('GameController', ['$scope', 'GameService', function($scope, GameService) {
+    var self = this;
+    self.round={count:null,moves:{},winner:'',output:''};
+
+    self.playRound = function(playersHand, gameModeId){
+        GameService.playRound(playersHand, gameModeId)
+            .then(
+                function(d) {
+                    self.round = d;
+                    console.log("Round result:", self.round);
+                },
+                function(errResponse){
+                    console.error('Error while playing a round. ' + errResponse);
+                }
+            );
+    };
+
+    self.play = function(playersHand, gameModeId) {
+        console.log('Playing a round with hand ' + playersHand
+            + ' and game mode ' + gameModeId);
+        self.playRound(playersHand, gameModeId);
+
+        self.reset();
+    };
+
+
+    self.reset = function(){
+        self.user={count:null,moves:{},winner:'',output:''};
+        //$scope.gameForm.$setPristine(); //reset Form
+    };
+
+}]);
