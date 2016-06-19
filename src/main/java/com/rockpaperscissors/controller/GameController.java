ea9e5a38013914c85f500cab93dff9527de2be62
StefanieStoppel <StefanieStoppel@users.noreply.github.com>
2016-06-19 15:41:37 +0200

    Return @ResponseBody and delete root mapping

--- src/main/java/com/rockpaperscissors/controller/GameController.java
+++ src/main/java/com/rockpaperscissors/controller/GameController.java
@@ -6,18 +6,14 @@ import com.rockpaperscissors.model.OutputTemplate;
 import com.rockpaperscissors.model.Round;
 import com.rockpaperscissors.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
 //TODO: how to send json data with angularjs https://hello-angularjs.appspot.com/angularjs-http-service-ajax-post-json-data-code-example
 //Todo: send request
 
-@Controller
+@RestController
 public class GameController {
 
     @Autowired
@@ -26,13 +22,8 @@ public class GameController {
     @Autowired
     private GameConfiguration gameConfiguration;
 
-    @RequestMapping(value = "/")
-    public String index(ModelMap map){
-        return "index";
-    }
-
     @RequestMapping(value = "/play/{gameModeId}", produces = "application/json")
-    public Round rockPaperScissors(@PathVariable int gameModeId, @RequestParam(value="hand", required = false) String playersHand) {
+    public @ResponseBody Round rockPaperScissors(@PathVariable int gameModeId, @RequestParam(value="hand", required = false) String playersHand) {
         Round round;
         if(GameConfiguration.isValidGameModeId(gameModeId)) {
             gameConfiguration.setGameMode(gameModeId);
@@ -43,10 +34,10 @@ public class GameController {
 
         if(GameConfiguration.isValidHand(playersHand)) {
             // Can potentially be empty
-            String computersChoice = HandFactory.getRandomValidHand(gameModeId);
-            if(GameConfiguration.isValidHand(computersChoice)) {
+            String computersHand = HandFactory.getRandomValidHand(gameModeId);
+            if(GameConfiguration.isValidHand(computersHand)) {
                 // Return an object of type Round to be displayed as JSON
-                round = gameService.playRound(playersHand, computersChoice);
+                round = gameService.playRound(playersHand, computersHand);
             } else {
                 round = new Round(-1, Collections.emptyMap(), OutputTemplate.ERROR_INVALID_CHOICE_COMPUTER);
             }
