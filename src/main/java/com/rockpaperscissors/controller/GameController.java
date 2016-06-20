14d41d02092da6092b01d7f8e7e3c544d0a19def
StefanieStoppel <StefanieStoppel@users.noreply.github.com>
2016-06-20 20:30:06 +0200

    Refactor GameController and add comments; Extend GameControllerTest

--- src/main/java/com/rockpaperscissors/controller/GameController.java
+++ src/main/java/com/rockpaperscissors/controller/GameController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
-//TODO: how to send json data with angularjs https://hello-angularjs.appspot.com/angularjs-http-service-ajax-post-json-data-code-example
-//Todo: send request
-
+/**
+ * GameController is a REST-Controller receiving requests to play the game.
+ */
 @RestController
 public class GameController {
 
@@ -22,30 +22,38 @@ public class GameController {
     @Autowired
     private GameConfiguration gameConfiguration;
 
+    /**
+     * Receive incoming requests on "/play/{gameModeId}?hand={playersHand} and check parameter validity.
+     * If all parameters are valid, calls GameService method to play a round of the game.
+     *
+     * @param gameModeId: The game mode to be played.
+     * @param playersHand: The player's choice of hand.
+     * @return Round: Returns a Round object specifying players' moves, winner and optional error message-
+     */
     @RequestMapping(value = "/play/{gameModeId}", produces = "application/json")
     public @ResponseBody Round rockPaperScissors(@PathVariable int gameModeId, @RequestParam(value="hand", required = false) String playersHand) {
         Round round;
         if(GameConfiguration.isValidGameModeId(gameModeId)) {
             gameConfiguration.setGameMode(gameModeId);
             gameService.setStrategyByGameMode(gameModeId);
-        } else {
-            round = new Round(-1, Collections.emptyMap());
-            round.setMessage(OutputTemplate.ERROR_INVALID_GAME_MODE);
-        }
 
-        if(GameConfiguration.isValidHand(playersHand)) {
-            // Can potentially be empty
-            String computersHand = HandFactory.getRandomValidHand(gameModeId);
-            if(GameConfiguration.isValidHand(computersHand)) {
-                // Return an object of type Round to be displayed as JSON
-                round = gameService.playRound(playersHand, computersHand);
+            if(GameConfiguration.isValidHand(playersHand)) {
+                // Can potentially be empty
+                String computersHand = HandFactory.getRandomValidHand(gameModeId);
+                if(GameConfiguration.isValidHand(computersHand)) {
+                    // Return an object of type Round to be displayed as JSON
+                    round = gameService.playRound(playersHand, computersHand);
+                } else {
+                    round = new Round(-1, Collections.emptyMap());
+                    round.setMessage(OutputTemplate.ERROR_INVALID_CHOICE_COMPUTER);
+                }
             } else {
                 round = new Round(-1, Collections.emptyMap());
-                round.setMessage(OutputTemplate.ERROR_INVALID_CHOICE_COMPUTER);
+                round.setMessage(OutputTemplate.ERROR_INVALID_CHOICE_PLAYER);
             }
         } else {
             round = new Round(-1, Collections.emptyMap());
-            round.setMessage(OutputTemplate.ERROR_INVALID_CHOICE_PLAYER);
+            round.setMessage(OutputTemplate.ERROR_INVALID_GAME_MODE);
         }
         return round;
     }
